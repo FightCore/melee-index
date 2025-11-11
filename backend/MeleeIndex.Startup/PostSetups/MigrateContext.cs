94d54ba68387815cc182c3d05f9374a35bef3bf3
Bart de Bever <bartdebever112@gmail.com>
2025-11-11 23:59:35 +0100

    feat: Start setting up auto-migration of context

---
+++ backend/MeleeIndex.Startup/PostSetups/MigrateContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace MeleeIndex.Startup.PostSetups;
+
+public static class MigrateContext
+{
+    public static async Task MigrationContext<TDbContext>(this IServiceProvider services, IConfiguration configuration)
+        where TDbContext : DbContext
+    {
+        if (!configuration.GetValue<bool>("MigrateDatabase"))
+        {
+            return;
+        }
+        
+        
+    }
+}
