fc1ba98762eb597aed65d3355bdfa38475fc840d
Bart de Bever <bartdebever112@gmail.com>
2026-02-04 21:37:44 +0100

    feat: Sort glossary items alphabetically

--- frontend/src/app/components/glossaries/glossary-item-list/glossary-item-list.component.ts
+++ frontend/src/app/components/glossaries/glossary-item-list/glossary-item-list.component.ts
@@ -1,5 +1,5 @@
 import { GlossaryItem } from '@/models/glossaries/glossary-item';
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ALIGN_FOOTER_CARD_WITH_HEADER_PT } from '@/app/utilities/passthroughs/card-passthroughs';
 import { Chip } from 'primeng/chip';
@@ -12,6 +12,10 @@ import { CharacterIconComponent } from '@/app/components/generic/character-icon/
   styleUrl: './glossary-item-list.component.scss',
 })
 export class GlossaryItemListComponent {
+  displayItems = computed(() => {
+    const items = this.glossaryItems();
+    return items.sort((a, b) => a.name.localeCompare(b.name));
+  });
   glossaryItems = input.required<GlossaryItem[]>();
   cardPtOptions = ALIGN_FOOTER_CARD_WITH_HEADER_PT;
 }
